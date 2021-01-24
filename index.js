41bf1f170a2d28c93a851e6123413f4348b847cc
Xuehua Chen <xuehua@gmail.com>
2021-01-24 14:32:00 -0800

    Added clean-blog files to our project.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.listen(3000, ()=> {
   console.log("App listening on port 3000")
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname +'/index.html')
+  res.sendFile(__dirname +'/pages/index.html')
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname +'/pages/about.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname +'/pages/contact.html')
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(__dirname +'/pages/post.html')
 })
