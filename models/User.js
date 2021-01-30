e79346c0e79f5c2f13e304661584be6b9a769250
Xuehua Chen <xuehua@gmail.com>
2021-01-30 09:22:29 -0800

    use mongoose-unique-validator for the User model.

--- models/User.js
+++ models/User.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema
-
-// const UserSchema = new Schema({
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
-// })
 
 const UserSchema = new Schema({
   username: {
@@ -25,6 +14,8 @@ const UserSchema = new Schema({
     required: true,
   }
 })
+
+UserSchema.plugin(uniqueValidator)
 
 UserSchema.pre('save', function(next) {
   const user = this
