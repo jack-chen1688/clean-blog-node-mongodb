02730f3f2e8b7e47c84b409adc900bb4a9250438
Xuehua Chen <xuehua@gmail.com>
2021-01-29 22:22:40 -0800

    Use the connect flash package to record the validation errors in storeUser.js and then display the validation errors in newUser.js.

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,7 +4,10 @@ const User = require("../models/User")
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
-      console.log(error)
+      const validationErrors = Object.keys(error.errors).map(
+        key => error.errors[key].message)
+      // Writen to the flash
+      req.flash('validationErrors', validationErrors)
       res.redirect('/auth/register')
     } else {
       res.redirect('/')
