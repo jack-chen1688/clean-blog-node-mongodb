01508fb58baf081e6549270a23aef9929ed7accb
Xuehua Chen <xuehua@gmail.com>
2021-01-30 10:54:35 -0800

    relating posts collection with users collection in BlogPost.js and also set userid in storePost.js.

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -3,10 +3,12 @@ const BlogPost = require('../models/BlogPost.js')
 module.exports = async (req, res) => {
   let image = req.files.image;
   // console.log(image)
+  console.log(req.session.userid)
   image.mv(__dirname + '/public/img/' + image.name, async (err) => {
     await BlogPost.create({
       ...req.body,
-      image: '/img/' + image.name
+      image: '/img/' + image.name,
+      userid: req.session.userId
     })
     res.redirect('/')
   })
