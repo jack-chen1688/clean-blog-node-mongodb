d326b224d1492de21b5e3a26157aba8319795516
Xuehua Chen <xuehua@gmail.com>
2021-01-30 11:08:55 -0800

    Use populate() to find out the corresponding user info. Use blogpost.userid.username in home.ejs and post.ejs to show the author of a post.

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,7 +2,7 @@ const BlogPost = require('../models/BlogPost.js')
 
 module.exports = async (req, res) => {
   // res.sendFile(__dirname +'/pages/post.html')
-  blogpost = await BlogPost.findById(req.params.id)
+  blogpost = await BlogPost.findById(req.params.id).populate('userid')
   res.render('post', {
     blogpost
   })
--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ const BlogPost = require('../models/BlogPost.js')
 
 module.exports = async (req, res) => {
   // res.sendFile(__dirname +'/pages/index.html')
-  const blogposts = await BlogPost.find({})
+  const blogposts = await BlogPost.find({}).populate('userid')
   // console.log(req.session)
   res.render('index', {
     blogposts // This means blogposts:blogposts
